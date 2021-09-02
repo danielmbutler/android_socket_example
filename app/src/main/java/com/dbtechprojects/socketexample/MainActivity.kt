4450e07bcd95cacb1197c8dac47b834fd86f1b2c
Daniel Butler <danielmbutler@hotmail.co.uk>
2021-09-02 17:24:45 +0100

    adding layouts

--- app/src/main/java/com/dbtechprojects/socketexample/MainActivity.kt
+++ app/src/main/java/com/dbtechprojects/socketexample/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.dbtechprojects.socketexample
 
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dbtechprojects.socketexample.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding get() = _binding!!
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.button.setOnClickListener {
+            val intent = Intent(this, ChatActivity::class.java)
+            intent.putExtra("name", binding.nameView.text.toString())
+            startActivity(intent)
+        }
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
